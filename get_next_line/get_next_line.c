1e9b6eed1100bbe8ec32ed5da341965f84b396c1
Khanh Ho Gia <kho-gia@c3r8s1.42prague.com>
2023-02-21 21:40:54 +0100

    wip get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,8 +6,34 @@
 /*   By: kho-gia <kho-gia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:08:40 by kho-gia           #+#    #+#             */
-/*   Updated: 2023/02/20 15:09:34 by kho-gia          ###   ########.fr       */
+/*   Updated: 2023/02/21 21:40:05 by kho-gia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	static_buffer[1000000];
+	char		line[1000000];
+	int			bread;
+	int			i;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	i = 0;
+	bread = read(fd, static_buffer, 1);
+	while (bread > 0)
+	{
+		line[i] = static_buffer[i];
+		if (static_buffer[i++] == '\n')
+			break ;
+		bread = read(fd, &static_buffer[i], 1);
+	}
+	line[i] = '\0';
+	if (bread <= 0 && i == 0)
+	{
+		return (NULL);
+	}
+	return (ft_strdup(line));
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kho-gia <kho-gia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:08:51 by kho-gia           #+#    #+#             */
-/*   Updated: 2023/02/20 15:39:42 by kho-gia          ###   ########.fr       */
+/*   Updated: 2023/02/21 21:39:53 by kho-gia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+#  define BUFFER_SIZE 10000
 # endif
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(char *src);
 
-# endif
+#endif
